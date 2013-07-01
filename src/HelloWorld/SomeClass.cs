19d1d2084866bd426dc4b2c6b55b0b34ae3a6b36
Brendan Forster <me@brendanforster.com>
2013-07-01 10:16:27 +0930

    addd a new file

---
+++ src/HelloWorld/SomeClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class SomeClass
+    {
+        private string _someProperty;
+        private int _anotherProperty;
+
+        public string SomeProperty
+        {
+            get { return _someProperty; }
+            set { _someProperty = value; }
+        }
+
+        public int AnotherProperty
+        {
+            get { return _anotherProperty; }
+            set { _anotherProperty = value; }
+        }
+    }
+}
