1dba947bf569e65bdc4d18e641838fcd04ebbd74
Brendan Forster <me@brendanforster.com>
2013-07-07 21:20:10 +0930

    added packages and setup test project

---
+++ src/Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Foo()
+        {
+            Assert.False(true);
+        }
+    }
+}
