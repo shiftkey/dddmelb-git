7a2b3cbe975b4c0f4cebccf50e6f22508576c567
Brendan Forster <me@brendanforster.com>
2013-07-14 15:53:50 +0930

    scripts because i'm lazy

---
+++ setup.bat
@@ -0,0 +1,9 @@
+@echo off
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.s "status"
+git config --global alias.d "diff"
+git config --global alias.ds "diff --staged"
+git config --global alias.now "rev-parse --short HEAD"
+
+git config color.status auto --global
+git config color.diff auto --global
