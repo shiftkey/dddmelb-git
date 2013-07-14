996c0af692a389cfa626dbe4326a432da21ad5e0
Brendan Forster <me@brendanforster.com>
2013-07-14 15:57:46 +0930

    whoops those %'s are important

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 @echo off
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.lg "log --color --graph --pretty=format:'%%Cred%%h%%Creset -%%C(yellow)%%d%%Creset %%s %%Cgreen(%%cr) %%C(bold blue)<%%an>%%Creset' --abbrev-commit"
 git config --global alias.s "status"
 git config --global alias.d "diff"
 git config --global alias.ds "diff --staged"
